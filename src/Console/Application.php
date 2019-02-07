8c318cee89d4327a561f2e8572121197093366cc
Benjamin Kott <benjamin.kott@outlook.com>
2019-02-08 00:02:34 +0100

    [TASK] Add base application

---
+++ src/Console/Application.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the bk2k/extension-helper.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace BK2K\ExtensionHelper\Console;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+/**
+ * Application
+ */
+class Application extends BaseApplication
+{
+    const VERSION = '1.0.0-DEV';
+
+    public function __construct()
+    {
+        parent::__construct('Extension Helper', self::VERSION);
+    }
+}
